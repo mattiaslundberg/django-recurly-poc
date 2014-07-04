e5b7ba257b0ff6970d811edb0c89e3cf01062748
Mattias Lundberg <lundberg.mattias@gmail.com>
2014-07-04 19:11:27 +0200

    Add unittest for getting recurly account.

--- myrecurly/subscription/tests.py
+++ myrecurly/subscription/tests.py
@@ -14,3 +14,16 @@ class TestAccountCreation(TestCase):
         u.save()
 
         self.assertEqual(act.save.call_count, 1, "Recurly account not saved")
+
+    def test_get(self):
+        act = recurly.Account
+        act.__init__ = Mock(return_value=None)
+        act.get = Mock(return_value=act)
+        act.save = Mock(return_value=None)
+
+        u = User(username="user2")
+        u.save()
+        u.save()
+
+        self.assertEqual(act.save.call_count, 2, "Recurly account not saved on update")
+        self.assertEqual(act.get.call_count, 1, "Recurly account getter not called")
