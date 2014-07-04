735931d8129f367dbf9820f64e071868a736ab3d
Mattias Lundberg <lundberg.mattias@gmail.com>
2014-07-04 19:06:25 +0200

    Add unittest for creation of recurly account.

--- myrecurly/subscription/tests.py
+++ myrecurly/subscription/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from mock import Mock
+import recurly
 
-# Create your tests here.
+class TestAccountCreation(TestCase):
+
+    def test_save(self):
+        act = recurly.Account
+        act.__init__ = Mock(return_value=None)
+        act.save = Mock(return_value=None)
+
+        u = User(username="user")
+        u.save()
+
+        self.assertEqual(act.save.call_count, 1, "Recurly account not saved")
